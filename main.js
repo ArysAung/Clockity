0cc6a8a0b030b6127025d6e1e82fcbfd7ad860f3
Arys Aung <arys.aung@gmail.com>
2016-07-20 12:06:14 -0400

    fixed 'purple' into 'random colors'

--- main.js
+++ main.js
@@ -12,17 +12,22 @@ function startTime() {
     var t = setTimeout(startTime, 10);
 }
 function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
+    if (i < 10) {
+        return i = "0" + i;
+        checkEven();
 }
+    else {// add zero in front of numbers < 10
+    return i;
+     checkEven();
+}}
 //Slight Bug, where color only changes on time! if you reload page as seconds hit a multiple of 10.
 function checkEven(){
-    var today = new Date()
+    var today = new Date();
     var i = today.getMinutes();
     if (i%2 == 0) {
        document.getElementById("txt").style.color = 'red';
         toParty();
-       setInterval(checkEven, 10000);
+        setInterval(checkEven, 10000);
       }
       else 
       {
@@ -32,11 +37,21 @@ function checkEven(){
       }
   }
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 function toParty(){
     var today = new Date()
     var i =  today.getMinutes()
     if (i%5 == 0) {
-        document.getElementById('body').style.backgroundColor = 'purple';
+    document.getElementById('body').style.backgroundColor = getRandomColor();
    }else{
-    document.getElementById('body').style.backgroundColor = 'white'
+    document.getElementById('body').style.backgroundColor = 'white';
+    checkTime();
    }}
